becf5fe7f2381bed35ae16717ad9b6afa221ab0b
Grigoriy Gavrilov <lowww1337@mail.ru>
2025-12-01 01:53:29 +0300

    add cursor pointer

--- src/components/BasicGrid/BasicGrid.tsx
+++ src/components/BasicGrid/BasicGrid.tsx
@@ -645,7 +645,7 @@ export function BasicGrid<RowType extends Record<string, unknown> = Record<strin
                   sortColumn={sortState?.columnId}
                   sortDirection={sortState?.direction}
                   onColumnSort={handleHeaderSort}
-                  debugMode
+                  // debugMode
                 />
               </div>
             </div>
